ebae2a970c6ba0b2b55173284be2329d112b04b9
unknown <gundavaramsahana@gmail.com>
2017-07-15 22:20:28 +0530

    modified

---
+++ lg.php
@@ -0,0 +1,36 @@
+<html>
+<?php 
+$con=mysqli_connect('localhost','root','','cfg14');
+$sname=$_POST['username'];
+ $pass=$_POST['password'];
+ $sql="select username,password from credentials"; 
+$r=mysqli_query($con,$sql); 
+
+    
+    while($row = $r->fetch_assoc())
+{
+if($sname==$row['username'] && $pass==$row['password']) 
+{ $flag=1; } 
+else
+{
+$flag=0;
+}
+}
+if($flag==1)
+{
+echo "success";
+header('location:style1.css');
+	
+}
+else
+ {
+	
+	header('location:u1.php');
+	
+	
+ }
+ 
+ 
+
+?>
+</html>
