64378a0cfe9b61819a21f4274e6009f5f31b074a
unknown <gundavaramsahana@gmail.com>
2017-07-15 16:05:34 +0530

    assessment tool

---
+++ assess.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6 ielt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7 ielt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+
+<title>Student Assessment</title>
+<link rel="stylesheet" type="text/css" href="style2.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+      <div class="jumbotron text-center">
+  <h1>DREAM LIFE SKILLS ASSESSMENT SCALE</h1>
+  </div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-2 col-xs-offset-2">
+      
+      <p>Does not net do</p>
+      
+    </div>
+    <div class="col-sm-2">
+     
+      <p>Does with lots of help</p>
+    
+    </div>
+    <div class="col-sm-2">
+      
+      <p>Does with some help</p>
+      
+    </div>
+	 <div class="col-sm-2">
+      
+      <p>Does with a little help</p>
+      
+    </div>
+	 <div class="col-sm-2">
+      
+      <p>Does indepndently</p>
+      
+    </div>
+  </div>
+</div>
+<div class="row">
+<div class="col-xs-2">
+
+   <p>IO Interacting with each other</p>
+   </div>
+   </div>
+   <div class="row">
+<div class="col-xs-2">
+
+   <p>DP Overcoming difficulties and solving problems</p>
+   </div>
+   </div>
+   <div class="row">
+<div class="col-xs-2">
+
+   <p>TL Taking initiative</p>
+   </div>
+   </div>
+   <div class="row">
+<div class="col-xs-2">
+
+   <p>MC Managing Conflict</p>
+   </div>
+   </div>
+   <div class="row">
+<div class="col-xs-2">
+
+   <p>UL Understanding and following insttructions</p>
+   </div>
+   </div>
+	   </body>
+	   </html>
