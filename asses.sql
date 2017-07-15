7bb72f074c3d6f53361efcc1b718be85a2c170ff
unknown <gundavaramsahana@gmail.com>
2017-07-16 04:30:02 +0530

    student assessment db

---
+++ asses.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 16, 2017 at 12:59 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cfg14`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asses`
+--
+
+CREATE TABLE `asses` (
+  `id` int(10) NOT NULL,
+  `1` int(10) NOT NULL,
+  `2` int(10) NOT NULL,
+  `3` int(10) NOT NULL,
+  `4` int(10) NOT NULL,
+  `5` int(10) NOT NULL,
+  `6` int(10) NOT NULL,
+  `7` int(10) NOT NULL,
+  `8` int(10) NOT NULL,
+  `9` int(10) NOT NULL,
+  `10` int(10) NOT NULL,
+  `11` int(10) NOT NULL,
+  `12` int(10) NOT NULL,
+  `13` int(10) NOT NULL,
+  `14` int(10) NOT NULL,
+  `15` int(10) NOT NULL,
+  `16` int(10) NOT NULL,
+  `17` int(10) NOT NULL,
+  `18` int(10) NOT NULL,
+  `19` int(10) NOT NULL,
+  `20` int(10) NOT NULL,
+  `21` int(10) NOT NULL,
+  `22` int(10) NOT NULL,
+  `23` int(10) NOT NULL,
+  `24` int(10) NOT NULL,
+  `25` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `asses`
+--
+
+INSERT INTO `asses` (`id`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`) VALUES
+(0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(0, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
