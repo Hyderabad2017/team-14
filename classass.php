797d23e7a28ff4b161249d219c81dc80a86bd298
unknown <gundavaramsahana@gmail.com>
2017-07-15 18:33:30 +0530

    class modified

---
+++ classass.php
@@ -0,0 +1,201 @@
+
+<html>
+<head>
+<title>Student inventory</title>
+<link rel="stylesheet" type="text/css" href="style2.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="jumbotron text-center">
+  <h1>My class inventory</h1>
+  
+  </div>
+   <div class="container">
+  <table class="table">
+<table>
+<thead>
+      <tr>
+        <th>    </th>
+        <th>YES</th>
+        <th>NO</th>
+		
+      </tr>
+    </thead>
+	<tbody>
+
+<tr>
+<td>1.Students enjoy their schoolwork in my class. </td>
+<td> <input type="radio"  id="q1.y" value="yes"> </td>
+  <td><input type="radio"  id="q1.n" value="no" ><br></td>
+</tr>
+<tr>
+<br>
+<td>2.Students are always fighting with each other.
+ </td>
+<td> <input type="radio" name="q2" value="yes"> </td>
+  <td><input type="radio" name="q2" value="no" ><br></td>
+</tr>
+<tr>
+
+  <td>3.Students often race to see who can finishfirst.</td>
+<td>
+<input type="radio" name="q3" value="yes"> </td>
+ <td> <input type="radio" name="q3" value="no"> <br></td>
+</tr>
+<tr>
+<td>4. In our class the work is hard to do.</td>
+<td><input type="radio" name="q4" value="yes"> </td>
+ <td> <input type="radio" name="q4" value="no"> <br></td>
+</tr>
+<tr>
+<td>5. In my class everyone is my friend.
+ </td>
+<td><input type="radio" name="q5" value="yes"> </td>
+<td><input type="radio" name="q5" value="no"> <br></td>
+</tr>
+  <tr>
+<td>6. Some students are not happy in class.</td>
+<td><input type="radio" name="q6" value="yes"> </td>
+<td>  <input type="radio" name="q6" value="no"> <br></td>
+</tr>
+<tr>
+<td> 7. Some of the students in our class are mean.
+</td>
+<td><input type="radio" name="q7" value="yes"> </td>
+<td> <input type="radio" name="q7" value="no"> <br></td>
+</tr>
+<tr>
+<td> 
+8. Most students want their work to be better than their friends' work.</td>
+<td><input type="radio" name="q8" value="yes"> </td>
+ <td><input type="radio" name="q8" value="no"> <br></td>
+</tr>
+<tr>
+<td>9. Most students can do their schoolwork without help.</td>
+
+<td><input type="radio" name="q9" value="yes"> </td>
+ <td><input type="radio" name="q9" value="no"> <br></td>
+</tr>
+<tr>
+<td>10. Some people in my class are not my friends.
+
+</td>
+<td><input type="radio" name="q10" value="yes"> </td>
+ <td><input type="radio" name="q10" value="no"> <br></td>
+</tr>
+<tr>
+<td>11. Students seem to like the class.</td>
+<td><input type="radio" name="q11" value="yes"> </td>
+<td> <input type="radio" name="q11" value="no"><br></td>
+</tr>
+<tr>
+<td>12. Many students in our class like to fight.
+</td>
+<td><input type="radio" name="q12" value="yes"> </td>
+ <td><input type="radio" name="q12" value="no"><br></td>
+</tr>
+<tr>
+<td>
+13. Some students feel bad when they don't do as well as the others.</td>
+<td>
+<input type="radio" name="q13" value="yes"> </td>
+<td> <input type="radio" name="q13" value="no"> <br></td>
+</tr>
+<tr>
+<td>14. Only the smarter students can do their work.</td>
+<td>
+<input type="radio" name="q14" value="yes"> </td>
+<td> <input type="radio" name="q14" value="no"> <br></td>
+</tr>
+<tr>
+<td>15. All students in my class are close friends.</td>
+<td>
+
+<input type="radio" name="q15" value="yes"> </td>
+ <td><input type="radio" name="q15" value="no"> <br></td>
+</tr>
+<tr>
+<td>16. Some of the students do not like the class.
+
+</td>
+<td><input type="radio" name="q16" value="yes"> </td>
+ <td><input type="radio" name="q16" value="no"> <br></td>
+</tr>
+<tr>
+<td>17. Certain students always want to have their way.</td>
+<td><input type="radio" name="q17" value="yes"> </td>
+ <td><input type="radio" name="q17" value="no"> <br></td>
+</tr>
+<tr>
+<td>18. Some students always try to do their work better than the others.
+</td>
+<td><input type="radio" name="q18" value="yes"> </td>
+ <td><input type="radio" name="q18" value="no"> <br></td>
+</tr>
+<tr>
+<td>19. Schoolwork is hard to do.</td>
+<td><input type="radio" name="q19" value="yes"> </td>
+ <td><input type="radio" name="q19" value="no"> <br></td>
+</tr>
+<tr>
+<td>
+20. All of the students in my class like each other.</td>
+<td><input type="radio" name="q20" value="yes"> </td>
+<td> <input type="radio" name="q20" value="no"> <br></td>
+</tr>
+<tr>
+<td>21. This class is fun.</td>
+
+
+<td><input type="radio" name="q21" value="yes"> </td>
+<td> <input type="radio" name="q21" value="no"> <br></td>
+</tr>
+<tr>
+<td>
+22. Students in our class fight a lot.</td>
+<td><input type="radio" name="q22" value="yes"> </td>
+<td> <input type="radio" name="q22" value="no"> <br></td>
+</tr>
+<tr>
+<td>23. A few students in my class want to be first all the time
+.</td>
+<td><input type="radio" name="q23" value="yes"> </td>
+ <td><input type="radio" name="q23" value="no"> <br></td>
+</tr>
+<tr>
+<td>24. Most of the students in my class know how to do their work.</td>
+<td><input type="radio" name="q24" value="yes"> </td>
+ <td><input type="radio" name="q24" value="no"> <br></td>
+</tr>
+<tr>
+<td>25. Students in our class like each other as friends.</td>
+<td><input type="radio" name="q25" value="yes"> </td>
+ <td><input type="radio" name="q25" value="no"> <br></td>
+</tr>
+</tbody>
+
+
+
+</table>
+<input type="button" onclick=action1()>
+<script>
+function  action1()
+{
+var a1=document.getElementById("q1.y").value;
+var a2=document.getElementById("q1.n").value;
+
+
+
+document.write(a1);
+document.write(a2);
+}
+</script>
+
+</body>
+</html>
