ffb5b5c5c7fd0126cd7c12e9b915683e4c4c435b
saikrishnakotnala <saikrishnachinnu8@gmail.com>
2017-07-15 21:34:50 +0530

    data.php

---
+++ data.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+<script>    .progress, .progress-bar{
+    height: 28px;
+}
+.progress-bar{
+	padding: 4px;
+}
+
+</script>
+    <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+    <div class="container">
+  <div class="row">
+    <h3>Progress Bar</h3>
+    <div class="progress">
+      	<div class="input-group">
+        	<span class="input-group-addon">0</span>
+      		<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+              <span>20%</span> (<span class="text-danger">1 </span>)
+  			</div>
+        	<span class="input-group-addon">5</span>
+    	</div>
+      <div class="clearfix"></div>
+	</div>
+  </div>
+</div>
+</html>
+
+
