6233cf14207ab1820738d5d5cb7eb7e311648ac2
unknown <gundavaramsahana@gmail.com>
2017-07-16 09:38:41 +0530

    Classroom assesment

--- classass.php
+++ classass.php
@@ -32,11 +32,85 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 @$w=$_POST['q23'];
 @$x=$_POST['q24'];
 @$y=$_POST['q25'];
-
+if(empty($a)){
+	$a=2;
+}
+if(empty($b)){
+	$b=2;
+}
+if(empty($c)){
+	$c=2;
+}
+if(empty($d)){
+	$d=2;
+}
+if(empty($e)){
+	$e=2;
+}
+if(empty($f)){
+	$f=2;
+}
+if(empty($g)){
+	$g=2;
+}
+if(empty($h)){
+	$h=2;
+}
+if(empty($i)){
+	$i=2;
+}
+if(empty($j)){
+	$j=2;
+}
+if(empty($k)){
+	$k=2;
+}
+if(empty($l)){
+	$l=2;
+}
+if(empty($m)){
+	$m=2;
+}
+if(empty($n)){
+	$n=2;
+}
+if(empty($o)){
+	$o=2;
+}
+if(empty($p)){
+	$p=2;
+}
+if(empty($q)){
+	$q=2;
+}
+if(empty($r)){
+	$r=2;
+}
+if(empty($s)){
+	$s=2;
+}
+if(empty($t)){
+	$t=2;
+}
+if(empty($u)){
+	$u=2;
+}
+if(empty($v)){
+	$v=2;
+}
+if(empty($w)){
+	$w=2;
+}
+if(empty($x)){
+	$x=2;
+}
+if(empty($y)){
+	$y=2;
+}
 if(@$_POST['submit'])
 { 
  $s="insert into asses(`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`10`,`11`,`12`,`13`,`14`,`15`,`16`,`17`,`18`,`19`,`20`,`21`,`22`,`23`,`24`,`25`) values($a,$b,$c,$d,$e,$f,$g,$h,$i,$j,$k,$l,$m,$n,$o,$p,$q,$r,$s,$t,$u,$v,$w,$x,$y);";
-echo $s;
+
 $conn->query($s);
 }
 ?> 
@@ -68,6 +142,7 @@ $conn->query($s);
         <th>YES</th>
         <th>NO</th>
 		
+		
       </tr>
     </thead>
 	<tbody>
@@ -76,13 +151,15 @@ $conn->query($s);
 <td>1.Students enjoy their schoolwork in my class. </td>
 <td> <input type="radio"  name="q1" value="3"> </td>
   <td><input type="radio"  name="q1" value="1" ><br></td>
+    
 </tr>
 <tr>
 <br>
 <td>2.Students are always fighting with each other.
  </td>
 <td> <input type="radio" name="q2" value="3"> </td>
-  <td><input type="radio" name="q2" value=1" ><br></td>
+  <td><input type="radio" name="q2" value="1" ><br></td>
+  
 </tr>
 <tr>
 
@@ -90,58 +167,68 @@ $conn->query($s);
 <td>
 <input type="radio" name="q3" value="3"> </td>
  <td> <input type="radio" name="q3" value="1"> <br></td>
+ 
 </tr>
 <tr>
 <td>4. In our class the work is hard to do.</td>
 <td><input type="radio" name="q4" value="3"> </td>
  <td> <input type="radio" name="q4" value="1"> <br></td>
+ 
 </tr>
 <tr>
 <td>5. In my class everyone is my friend.
  </td>
 <td><input type="radio" name="q5" value="3"> </td>
 <td><input type="radio" name="q5" value="1"> <br></td>
+
 </tr>
   <tr>
 <td>6. Some students are not happy in class.</td>
-<td><input type="radio" name="q6" value="3"> </td>
-<td>  <input type="radio" name="q6" value="1"> <br></td>
+<td><input type="radio" name="q6" value="1"> </td>
+<td>  <input type="radio" name="q6" value="3"> <br></td>
+
 </tr>
 <tr>
 <td> 7. Some of the students in our class are mean.
 </td>
 <td><input type="radio" name="q7" value="3"> </td>
 <td> <input type="radio" name="q7" value="1"> <br></td>
+
 </tr>
 <tr>
 <td> 
 8. Most students want their work to be better than their friends' work.</td>
 <td><input type="radio" name="q8" value="3"> </td>
  <td><input type="radio" name="q8" value="1"> <br></td>
+ 
 </tr>
 <tr>
 <td>9. Most students can do their schoolwork without help.</td>
 
-<td><input type="radio" name="q9" value="3"> </td>
- <td><input type="radio" name="q9" value="1"> <br></td>
+<td><input type="radio" name="q9" value="1"> </td>
+ <td><input type="radio" name="q9" value="3"> <br></td>
+ 
 </tr>
 <tr>
 <td>10. Some people in my class are not my friends.
 
 </td>
-<td><input type="radio" name="q10" value="3"> </td>
- <td><input type="radio" name="q10" value="1"> <br></td>
+<td><input type="radio" name="q10" value="1"> </td>
+ <td><input type="radio" name="q10" value="3"> <br></td>
+ 
 </tr>
 <tr>
 <td>11. Students seem to like the class.</td>
 <td><input type="radio" name="q11" value="3"> </td>
 <td> <input type="radio" name="q11" value="1"><br></td>
+
 </tr>
 <tr>
 <td>12. Many students in our class like to fight.
 </td>
 <td><input type="radio" name="q12" value="3"> </td>
  <td><input type="radio" name="q12" value="1"><br></td>
+ 
 </tr>
 <tr>
 <td>
@@ -149,12 +236,14 @@ $conn->query($s);
 <td>
 <input type="radio" name="q13" value="3"> </td>
 <td> <input type="radio" name="q13" value="1"> <br></td>
+
 </tr>
 <tr>
 <td>14. Only the smarter students can do their work.</td>
 <td>
 <input type="radio" name="q14" value="3"> </td>
 <td> <input type="radio" name="q14" value="1"> <br></td>
+
 </tr>
 <tr>
 <td>15. All students in my class are close friends.</td>
@@ -162,35 +251,41 @@ $conn->query($s);
 
 <input type="radio" name="q15" value="3"> </td>
  <td><input type="radio" name="q15" value="1"> <br></td>
+ 
 </tr>
 <tr>
 <td>16. Some of the students do not like the class.
 
 </td>
-<td><input type="radio" name="q16" value="3"> </td>
- <td><input type="radio" name="q16" value="1"> <br></td>
+<td><input type="radio" name="q16" value="1"> </td>
+ <td><input type="radio" name="q16" value="3"> <br></td>
+ 
 </tr>
 <tr>
 <td>17. Certain students always want to have their way.</td>
 <td><input type="radio" name="q17" value="3"> </td>
  <td><input type="radio" name="q17" value="1"> <br></td>
+ 
 </tr>
 <tr>
 <td>18. Some students always try to do their work better than the others.
 </td>
 <td><input type="radio" name="q18" value="3"> </td>
  <td><input type="radio" name="q18" value="1"> <br></td>
+ 
 </tr>
 <tr>
 <td>19. Schoolwork is hard to do.</td>
 <td><input type="radio" name="q19" value="3"> </td>
  <td><input type="radio" name="q19" value="1"> <br></td>
+ 
 </tr>
 <tr>
 <td>
 20. All of the students in my class like each other.</td>
 <td><input type="radio" name="q20" value="3"> </td>
 <td> <input type="radio" name="q20" value="1"> <br></td>
+
 </tr>
 <tr>
 <td>21. This class is fun.</td>
@@ -198,28 +293,33 @@ $conn->query($s);
 
 <td><input type="radio" name="q21" value="3"> </td>
 <td> <input type="radio" name="q21" value="1"> <br></td>
+
 </tr>
 <tr>
 <td>
 22. Students in our class fight a lot.</td>
 <td><input type="radio" name="q22" value="3"> </td>
 <td> <input type="radio" name="q22" value="1"> <br></td>
+
 </tr>
 <tr>
 <td>23. A few students in my class want to be first all the time
 .</td>
 <td><input type="radio" name="q23" value="3"> </td>
  <td><input type="radio" name="q23" value="1"> <br></td>
+
 </tr>
 <tr>
 <td>24. Most of the students in my class know how to do their work.</td>
-<td><input type="radio" name="q24" value="3"> </td>
- <td><input type="radio" name="q24" value="1"> <br></td>
+<td><input type="radio" name="q24" value="1"> </td>
+ <td><input type="radio" name="q24" value="3"> <br></td>
+ 
 </tr>
 <tr>
 <td>25. Students in our class like each other as friends.</td>
 <td><input type="radio" name="q25" value="3"> </td>
  <td><input type="radio" name="q25" value="1"> <br></td>
+ 
 </tr>
 </tbody>
 
